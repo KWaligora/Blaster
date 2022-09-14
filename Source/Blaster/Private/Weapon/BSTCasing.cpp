4a143f358d80557199dda4b11c71bf15ada0059d
Kamil Waligóra <k.waligora37@gmail.com>
2022-09-14 07:07:32 +0200

    Add physics to bullet shells.

--- Source/Blaster/Private/Weapon/BSTCasing.cpp
+++ Source/Blaster/Private/Weapon/BSTCasing.cpp
@@ -1,16 +1,43 @@
 ﻿#include "Weapon/BSTCasing.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
+
 
 ABSTCasing::ABSTCasing()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
+	CasingMesh->SetCollisionResponseToChannel(ECC_Camera, ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+	
 	SetRootComponent(CasingMesh);
+}
+
+void ABSTCasing::PostInitializeComponents()
+{
+	Super::PostInitializeComponents();
+
+	CasingMesh->OnComponentHit.AddDynamic(this, &ABSTCasing::OnHit);
 }
 
 void ABSTCasing::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+}
+
+void ABSTCasing::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound != nullptr)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
 	
+	Destroy();
 }
--- Source/Blaster/Public/Weapon/BSTCasing.h
+++ Source/Blaster/Public/Weapon/BSTCasing.h
@@ -13,9 +13,19 @@ public:
 	ABSTCasing();
 
 protected:
+	virtual void PostInitializeComponents() override;
 	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
 private:
 	UPROPERTY(VisibleAnywhere)
 	UStaticMeshComponent* CasingMesh;
+
+	UPROPERTY(EditAnywhere)
+	float ShellEjectionImpulse = 10.0f;
+
+	UPROPERTY(EditAnywhere)
+	class USoundCue* ShellSound;
 };
